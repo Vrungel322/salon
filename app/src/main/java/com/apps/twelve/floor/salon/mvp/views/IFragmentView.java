31f814b97c0832786a0897e19244baa4204c0a78
Vrungel <nikita241296@gmail.com>
2017-01-25 14:27:38 +0200

    base view interfaces add

---
+++ app/src/main/java/com/apps/twelve/floor/salon/mvp/views/IFragmentView.java
@@ -0,0 +1,10 @@
+package com.apps.twelve.floor.salon.mvp.views;
+
+import com.arellomobile.mvp.MvpView;
+
+/**
+ * Created by Vrungel on 25.01.2017.
+ */
+
+public interface IFragmentView extends MvpView {
+}
