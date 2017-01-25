c350ddda439c79513f2d61f0391d2ff1b2d683e0
Vrungel <nikita241296@gmail.com>
2017-01-25 14:28:14 +0200

    MainActivity view add

---
+++ app/src/main/java/com/apps/twelve/floor/salon/mvp/views/IMainActivityView.java
@@ -0,0 +1,13 @@
+package com.apps.twelve.floor.salon.mvp.views;
+
+import com.apps.twelve.floor.salon.mvp.views.IActivityView;
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
+
+/**
+ * Created by Vrungel on 25.01.2017.
+ */
+
+@StateStrategyType(AddToEndSingleStrategy.class)
+public interface IMainActivityView extends IActivityView {
+}
