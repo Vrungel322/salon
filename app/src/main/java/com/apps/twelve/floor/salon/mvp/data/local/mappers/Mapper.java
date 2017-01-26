695e068cfeec1f39f680baa74d65215d16a6f940
Vrungel <nikita241296@gmail.com>
2017-01-26 12:01:33 +0200

    mapper interface add

---
+++ app/src/main/java/com/apps/twelve/floor/salon/mvp/data/local/mappers/Mapper.java
@@ -0,0 +1,9 @@
+package com.apps.twelve.floor.salon.mvp.data.local.mappers;
+
+/**
+ * Created by Vrungel on 26.01.2017.
+ */
+
+public interface Mapper<A, B> {
+  B transform (A obj) throws RuntimeException;
+}
