c57a0026b96e610bc232839612e30bf14deedfb4
Alexey Moroz <udtalexey@gmail.com>
2017-01-27 16:27:52 +0200

    Add interface for navigator and implemented part methods

---
+++ app/src/main/java/com/apps/twelve/floor/salon/ui/base/INavigator.java
@@ -0,0 +1,49 @@
+package com.apps.twelve.floor.salon.ui.base;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by John on 27.01.2017.
+ */
+
+public interface INavigator {
+
+  //for activity
+  void finishActivity(@NonNull Activity activity);
+
+  void startActivityClearStack(@NonNull Activity activity, @NonNull Intent intent);
+
+  void startActivity(@NonNull Intent intent);
+
+  void startActivity(@NonNull String action);
+
+  void startActivity(@NonNull String action, @NonNull Uri uri);
+
+  void startActivityForResult(@NonNull Activity activity, @NonNull Intent intent, int requestCode);
+
+  //for fragment
+  void addFragment(@NonNull Activity activity, @IdRes int containerId, @NonNull Fragment fragment);
+
+  void addFragmentAndAddToBackStack(@NonNull Activity activity, @IdRes int containerId,
+      @NonNull Fragment fragment);
+
+  void addFragmentTagAndAddToBackStack(@NonNull Activity activity, @IdRes int containerId,
+      @NonNull Fragment fragment, @NonNull String fragmentTag);
+
+  void replaceFragment(@IdRes int containerId, @NonNull Fragment fragment, Bundle args);
+
+  void replaceFragment(@IdRes int containerId, @NonNull Fragment fragment,
+      @NonNull String fragmentTag, Bundle args);
+
+  void replaceFragmentAndAddToBackStack(@IdRes int containerId, @NonNull Fragment fragment,
+      Bundle args, String backstackTag);
+
+  void replaceFragmentAndAddToBackStack(@IdRes int containerId, @NonNull Fragment fragment,
+      @NonNull String fragmentTag, Bundle args, String backstackTag);
+}
--- app/src/main/java/com/apps/twelve/floor/salon/ui/base/Navigator.java
+++ app/src/main/java/com/apps/twelve/floor/salon/ui/base/Navigator.java
@@ -1,5 +1,14 @@
 package com.apps.twelve.floor.salon.ui.base;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -7,14 +16,90 @@ import javax.inject.Singleton;
  * Created by Vrungel on 26.01.2017.
  */
 
-@Singleton
-public class Navigator {
+@Singleton public class Navigator implements INavigator {
 
-  @Inject public Navigator() {
+  private Context mContext;
+
+  @Inject public Navigator(Context context) {
+    mContext = context;
   }
 
-  //public void startTestActivity(Context context){
-  //  Intent intent = new Intent(context, SomeActivity.class);
-  //  context.startActivity(intent);
-  //}
+  //for activity
+  @Override public void finishActivity(@NonNull Activity activity) {
+    activity.finish();
+  }
+
+  @Override
+  public void startActivityClearStack(@NonNull Activity activity, @NonNull Intent intent) {
+    activity.startActivity(intent);
+    activity.finishAffinity();
+  }
+
+  @Override public void startActivity(@NonNull Intent intent) {
+    mContext.startActivity(intent);
+  }
+
+  @Override public void startActivity(@NonNull String action) {
+    mContext.startActivity(new Intent(action));
+  }
+
+  @Override public void startActivity(@NonNull String action, @NonNull Uri uri) {
+    mContext.startActivity(new Intent(action, uri));
+  }
+
+  @Override public void startActivityForResult(@NonNull Activity activity, @NonNull Intent intent,
+      int requestCode) {
+    activity.startActivityForResult(intent, requestCode);
+  }
+
+  //for fragment
+  @Override public void addFragment(@NonNull Activity activity, @IdRes int containerId,
+      @NonNull Fragment fragment) {
+    ((FragmentActivity) activity).getSupportFragmentManager()
+        .beginTransaction()
+        .add(containerId, fragment)
+        .commit();
+  }
+
+  @Override
+  public void addFragmentAndAddToBackStack(@NonNull Activity activity, @IdRes int containerId,
+      @NonNull Fragment fragment) {
+    ((FragmentActivity) activity).getSupportFragmentManager()
+        .beginTransaction()
+        .addToBackStack(null)
+        .add(containerId, fragment)
+        .commit();
+  }
+
+  @Override
+  public void addFragmentTagAndAddToBackStack(@NonNull Activity activity, @IdRes int containerId,
+      @NonNull Fragment fragment, @NonNull String fragmentTag) {
+    ((FragmentActivity) activity).getSupportFragmentManager()
+        .beginTransaction()
+        .addToBackStack(null)
+        .add(containerId, fragment)
+        .commit();
+  }
+
+  @Override
+  public void replaceFragment(@IdRes int containerId, @NonNull Fragment fragment, Bundle args) {
+
+  }
+
+  @Override public void replaceFragment(@IdRes int containerId, @NonNull Fragment fragment,
+      @NonNull String fragmentTag, Bundle args) {
+
+  }
+
+  @Override
+  public void replaceFragmentAndAddToBackStack(@IdRes int containerId, @NonNull Fragment fragment,
+      Bundle args, String backstackTag) {
+
+  }
+
+  @Override
+  public void replaceFragmentAndAddToBackStack(@IdRes int containerId, @NonNull Fragment fragment,
+      @NonNull String fragmentTag, Bundle args, String backstackTag) {
+
+  }
 }
