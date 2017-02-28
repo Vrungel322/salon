f1ca8a9f769883bc3d7894fa242b4d5e3b810ce7
Alexey Moroz <udtalexey@gmail.com>
2017-02-28 13:52:59 +0200

    Add custom icon in Toolbar menu

--- app/src/main/java/com/apps/twelve/floor/salon/ui/activities/SettingsActivity.java
+++ app/src/main/java/com/apps/twelve/floor/salon/ui/activities/SettingsActivity.java
@@ -15,20 +15,6 @@ public class SettingsActivity extends BaseActivity {
     super.onCreate(savedInstanceState);
   }
 
-  @Override public boolean onCreateOptionsMenu(Menu menu) {
-    getMenuInflater().inflate(R.menu.menu, menu);
-    return true;
-  }
-
-  @Override public boolean onOptionsItemSelected(MenuItem item) {
-    switch (item.getItemId()) {
-      case R.id.action_settings:
-        return true;
-      default:
-        return super.onOptionsItemSelected(item);
-    }
-  }
-
   @Override public void onBackPressed() {
     super.onBackPressed();
     finish();
