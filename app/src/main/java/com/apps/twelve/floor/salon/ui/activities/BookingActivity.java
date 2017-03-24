416199f72e7db9aef59c92d9a072373e7cc40797
Alexey Moroz <udtalexey@gmail.com>
2017-03-24 15:32:47 +0200

    Fix shortcut

--- app/src/main/java/com/apps/twelve/floor/salon/ui/activities/BookingActivity.java
+++ app/src/main/java/com/apps/twelve/floor/salon/ui/activities/BookingActivity.java
@@ -8,11 +8,13 @@ import com.apps.twelve.floor.salon.mvp.views.IBookingActivityView;
 import com.apps.twelve.floor.salon.ui.base.BaseActivity;
 import com.apps.twelve.floor.salon.ui.fragments.BookingFragment;
 import com.arellomobile.mvp.presenter.InjectPresenter;
+import shortbread.Shortcut;
 
 /**
  * Created by John on 23.03.2017.
  */
 
+@Shortcut(id = "booking", icon = R.drawable.ic_shortcut_border_color, shortLabelRes = R.string.book_create)
 public class BookingActivity extends BaseActivity implements IBookingActivityView {
 
   @InjectPresenter BookingActivityPresenter mBookingActivityPresenter;
--- app/src/main/java/com/apps/twelve/floor/salon/ui/activities/SettingsActivity.java
+++ app/src/main/java/com/apps/twelve/floor/salon/ui/activities/SettingsActivity.java
@@ -3,9 +3,7 @@ package com.apps.twelve.floor.salon.ui.activities;
 import android.os.Bundle;
 import com.apps.twelve.floor.salon.R;
 import com.apps.twelve.floor.salon.ui.base.BaseActivity;
-import shortbread.Shortcut;
 
-@Shortcut(id = "settings", icon = R.drawable.ic_action_settings, shortLabel = "Settings")
 public class SettingsActivity extends BaseActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
