7ab25b0efe97a42adb74e279714084b3a65de14c
tinkooladik <tinkooladik@gmail.com>
2017-03-28 14:19:47 +0300

    added some listeners to booking contact fragment

--- app/src/main/java/com/apps/twelve/floor/salon/ui/fragments/BookingContactFragment.java
+++ app/src/main/java/com/apps/twelve/floor/salon/ui/fragments/BookingContactFragment.java
@@ -2,7 +2,10 @@ package com.apps.twelve.floor.salon.ui.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -25,6 +28,10 @@ public class BookingContactFragment extends BaseFragment implements IBookingCont
   @BindView(R.id.tv_time_duration) TextView mTextViewDuration;
   @BindView(R.id.tv_master_description) TextView mTextViewMaster;
   @BindView(R.id.tv_master_details) TextView mTextViewMasterDetails;
+  @BindView(R.id.edit_name) EditText mEditTextName;
+  @BindView(R.id.edit_phone) EditText mEditTextPhone;
+
+  private String mName, mPhone;
 
   public static BookingContactFragment newInstance() {
     Bundle args = new Bundle();
@@ -43,9 +50,35 @@ public class BookingContactFragment extends BaseFragment implements IBookingCont
 
     mTextViewService.setText(
         getArguments().getString(Constants.FragmentsArgumentKeys.SERVICE_NAME));
+
+    mEditTextName.addTextChangedListener(new TextWatcher() {
+
+      @Override public void afterTextChanged(Editable s) {
+        mName = s.toString();
+      }
+
+      @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+      }
+
+      @Override public void onTextChanged(CharSequence s, int start, int before, int count) {
+      }
+    });
+
+    mEditTextPhone.addTextChangedListener(new TextWatcher() {
+
+      @Override public void afterTextChanged(Editable s) {
+        mPhone = s.toString();
+      }
+
+      @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+      }
+
+      @Override public void onTextChanged(CharSequence s, int start, int before, int count) {
+      }
+    });
   }
 
   @OnClick(R.id.btn_booking_contact) void createBooking() {
-    showToastMessage("Записаться");
+    showToastMessage("Записаться\n" + mName + "\n" + mPhone);
   }
 }
