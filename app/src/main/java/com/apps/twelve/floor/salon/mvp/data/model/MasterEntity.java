ec94bf458d7307b11a1e5d848ead198a24ab08b2
Vrungel <nikita241296@gmail.com>
2017-04-04 14:21:32 +0300

    third transition

--- app/src/main/java/com/apps/twelve/floor/salon/mvp/data/model/MasterEntity.java
+++ app/src/main/java/com/apps/twelve/floor/salon/mvp/data/model/MasterEntity.java
@@ -8,11 +8,14 @@ public class MasterEntity {
   private String masterName;
   private String masterImg;
   private String masterDescription;
+  private String masterId;
 
-  public MasterEntity(String masterName, String masterImg, String masterDescription) {
+  public MasterEntity(String masterName, String masterImg, String masterDescription,
+      String masterId) {
     this.masterName = masterName;
     this.masterImg = masterImg;
     this.masterDescription = masterDescription;
+    this.masterId = masterId;
   }
 
   public String getMasterName() {
@@ -37,5 +40,13 @@ public class MasterEntity {
 
   public void setMasterDescription(String masterDescription) {
     this.masterDescription = masterDescription;
+  }
+
+  public String getMasterId() {
+    return masterId;
+  }
+
+  public void setMasterId(String masterId) {
+    this.masterId = masterId;
   }
 }
--- app/src/main/java/com/apps/twelve/floor/salon/mvp/presenters/pr_fragments/ChooseMasterFragmentPresenter.java
+++ app/src/main/java/com/apps/twelve/floor/salon/mvp/presenters/pr_fragments/ChooseMasterFragmentPresenter.java
@@ -3,12 +3,14 @@ package com.apps.twelve.floor.salon.mvp.presenters.pr_fragments;
 import com.apps.twelve.floor.salon.App;
 import com.apps.twelve.floor.salon.mvp.data.DataManager;
 import com.apps.twelve.floor.salon.mvp.data.model.BookingEntity;
+import com.apps.twelve.floor.salon.mvp.data.model.MasterEntity;
 import com.apps.twelve.floor.salon.mvp.presenters.BasePresenter;
 import com.apps.twelve.floor.salon.mvp.views.IChooseMasterFragmentView;
 import com.apps.twelve.floor.salon.utils.RxBus;
 import com.apps.twelve.floor.salon.utils.RxBusHelper;
 import com.apps.twelve.floor.salon.utils.ThreadSchedulers;
 import com.arellomobile.mvp.InjectViewState;
+import java.util.List;
 import javax.inject.Inject;
 import rx.Subscription;
 
@@ -21,6 +23,7 @@ import rx.Subscription;
   @Inject DataManager mDataManager;
   @Inject RxBus mRxBus;
   @Inject BookingEntity mBookingEntity;
+  private List<MasterEntity> mMasterEntities;
 
   @Override protected void inject() {
     App.getBookingComponent().inject(this);
@@ -37,9 +40,15 @@ import rx.Subscription;
         .compose(ThreadSchedulers.applySchedulers())
         .concatMap(dataID -> mDataManager.fetchMasters(dataID.dataId))
         .subscribe(masterEntities -> {
+          mMasterEntities = masterEntities;
           getViewState().showMasters(masterEntities);
           getViewState().hideProgressBar();
         });
     addToUnsubscription(subscription);
+  }
+
+  public void setSelectedItem(int position) {
+    mBookingEntity.setMasterId(mMasterEntities.get(position).getMasterId());
+    getViewState().setSelectedItem(position);
   }
 }
