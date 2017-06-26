c7b8b3061a68750fd7000b2cba4ce6faffd5da97
Alexey Moroz <udtalexey@gmail.com>
2017-06-26 14:26:38 +0300

    Add auth for response goods favorite

--- app/src/main/java/com/apps/twelve/floor/salon/feature/catalog/presenters/CatalogFavoriteFragmentPresenter.java
+++ app/src/main/java/com/apps/twelve/floor/salon/feature/catalog/presenters/CatalogFavoriteFragmentPresenter.java
@@ -1,13 +1,19 @@
 package com.apps.twelve.floor.salon.feature.catalog.presenters;
 
+import com.apps.twelve.floor.authorization.utils.AuthRxBusHelper;
 import com.apps.twelve.floor.salon.App;
 import com.apps.twelve.floor.salon.base.BasePresenter;
 import com.apps.twelve.floor.salon.feature.catalog.views.ICataloFavoriteFragmentView;
 import com.apps.twelve.floor.salon.utils.RxBusHelper;
 import com.apps.twelve.floor.salon.utils.ThreadSchedulers;
 import com.arellomobile.mvp.InjectViewState;
+import rx.Observable;
 import rx.Subscription;
 import timber.log.Timber;
+
+import static com.apps.twelve.floor.authorization.utils.Constants.Remote.RESPONSE_TOKEN_EXPIRED;
+import static com.apps.twelve.floor.authorization.utils.Constants.Remote.RESPONSE_UNAUTHORIZED;
+import static com.apps.twelve.floor.salon.utils.Constants.StatusCode.RESPONSE_200;
 
 /**
  * Created by John on 06.06.2017.
@@ -28,18 +34,26 @@ import timber.log.Timber;
   }
 
   private void fetchFavoriteGoodsList() {
-    Subscription subscription = mDataManager.fetchFavoriteGoods()
-        .compose(ThreadSchedulers.applySchedulers())
-        .subscribe(listResponse -> {
-          if (listResponse.code() != 401) {
-            getViewState().stopProgressBar();
-            getViewState().updateGoodsFavoriteList(listResponse.body());
-          } else {
-            //mAuthorizationManager.refreshToken();
+    Subscription subscription =
+        mAuthorizationManager.checkToken(mDataManager.fetchFavoriteGoods()).concatMap(response -> {
+          if (response.code() == RESPONSE_TOKEN_EXPIRED) {
+            return mAuthorizationManager.checkToken(mDataManager.fetchFavoriteGoods());
           }
-          if (listResponse.code() == 500) {
-            mAuthorizationManager.clear();
-            getViewState().startLoginActivity();
+          return Observable.just(response);
+        }).compose(ThreadSchedulers.applySchedulers()).subscribe(response -> {
+          switch (response.code()) {
+            case RESPONSE_200:
+              getViewState().stopProgressBar();
+              getViewState().updateGoodsFavoriteList(response.body());
+              break;
+            case RESPONSE_UNAUTHORIZED:
+              mAuthorizationManager.getAuthRxBus().post(new AuthRxBusHelper.UnauthorizedEvent());
+              getViewState().stopProgressBar();
+              break;
+            default:
+              getViewState().stopProgressBar();
+              showMessageException();
+              break;
           }
         }, throwable -> {
           getViewState().stopProgressBar();
