04d8fd92888c1e779425c57c00f2fd856c032d73
Alexey Moroz <udtalexey@gmail.com>
2017-06-29 13:40:58 +0300

    Fix style

--- app/src/main/java/com/apps/twelve/floor/salon/feature/start_point/activities/MainActivity.java
+++ app/src/main/java/com/apps/twelve/floor/salon/feature/start_point/activities/MainActivity.java
@@ -2,27 +2,21 @@ package com.apps.twelve.floor.salon.feature.start_point.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ImageView;
-import butterknife.BindView;
 import com.apps.twelve.floor.salon.R;
 import com.apps.twelve.floor.salon.base.BaseActivity;
 import com.apps.twelve.floor.salon.feature.start_point.presenters.MainActivityPresenter;
 import com.apps.twelve.floor.salon.feature.start_point.views.IMainActivityView;
 import com.apps.twelve.floor.salon.utils.ThemeUtils;
 import com.arellomobile.mvp.presenter.InjectPresenter;
-import com.bumptech.glide.Glide;
 
 public class MainActivity extends BaseActivity implements IMainActivityView {
 
   @InjectPresenter MainActivityPresenter mainActivityPresenter;
 
-  @BindView(R.id.ivSplash) ImageView mImageViewSplash;
-
   @Override protected void onCreate(Bundle savedInstanceState) {
     setTheme(ThemeUtils.getThemeNoActionBar(getBaseContext()));
     setContentView(R.layout.activity_main);
     super.onCreate(savedInstanceState);
-    Glide.with(this).load(R.drawable.splash).into(mImageViewSplash);
   }
 
   @Override public void afterSplash() {
