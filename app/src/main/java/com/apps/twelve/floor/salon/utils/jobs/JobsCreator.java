02267a97427e151ab77cdc343c4fa3e3e2e64e73
alexandersvyatetsky <alexswyat@gmail.com>
2017-07-27 17:50:37 +0300

    изменение времени уведомления для уже созданных

--- app/src/main/java/com/apps/twelve/floor/salon/utils/jobs/JobsCreator.java
+++ app/src/main/java/com/apps/twelve/floor/salon/utils/jobs/JobsCreator.java
@@ -3,11 +3,14 @@ package com.apps.twelve.floor.salon.utils.jobs;
 import android.content.Context;
 import com.apps.twelve.floor.salon.App;
 import com.apps.twelve.floor.salon.data.DataManager;
+import com.apps.twelve.floor.salon.data.model.LastBookingEntity;
+import com.apps.twelve.floor.salon.utils.Converters;
 import com.evernote.android.job.Job;
 import com.evernote.android.job.JobCreator;
 import com.evernote.android.job.JobManager;
 import com.evernote.android.job.JobRequest;
 import com.evernote.android.job.util.support.PersistableBundleCompat;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import javax.inject.Inject;
 
@@ -67,6 +70,18 @@ public class JobsCreator implements JobCreator {
         .setRequiredNetworkType(JobRequest.NetworkType.ANY)
         .build()
         .schedule();
+  }
+
+  public void updateNotifications() {
+    List<LastBookingEntity> bookingEntities = mDataManager.getBooking();
+    for (LastBookingEntity bookingEntity : bookingEntities) {
+      cancelJob(String.valueOf(bookingEntity.getId()));
+      createNotification(String.valueOf(bookingEntity.getId()),
+          bookingEntity.getServiceTime() * 1000L - System.currentTimeMillis(),
+          bookingEntity.getServiceName(),
+          Converters.detailDayFromSeconds(mContext, String.valueOf(bookingEntity.getServiceTime())),
+          Converters.timeFromSeconds(mContext, String.valueOf(bookingEntity.getServiceTime())));
+    }
   }
 
   public void cancelJob(String tag) {
